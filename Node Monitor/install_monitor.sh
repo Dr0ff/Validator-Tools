d0b257304983eda349e99f46bc98b76010a70158
TrustTech <161213942+Dr0ff@users.noreply.github.com>
2025-06-23 23:03:55 +0100

    Update install_monitor.sh

--- Node Monitor/install_monitor.sh
+++ Node Monitor/install_monitor.sh
@@ -21,45 +21,45 @@ MONITOR_INSTALL_DIR="$HOME/node_monitor"
 MONITOR_SCRIPT_URL="https://raw.githubusercontent.com/Dr0ff/Validator-Tools/refs/heads/main/Node%20Monitor/multi_node_monitor.sh" 
 MONITOR_SCRIPT_NAME="node_monitor.sh"
 
-echo -e "Начинаем установку монитора нод.....\n"
+echo -e "  Начинаем установку монитора нод.....\n"
 
 # 1. Проверяем и создаем директорию
 if [ ! -d "$MONITOR_INSTALL_DIR" ]; then
-    echo "Директория ${MONITOR_INSTALL_DIR} не найдена. Создаю..."
+    echo "  Директория ${MONITOR_INSTALL_DIR} не найдена. Создаю..."
     mkdir -p "$MONITOR_INSTALL_DIR" || { echo "Ошибка: Не удалось создать директорию ${MONITOR_INSTALL_DIR}. Проверьте права."; exit 1; }
-    echo "Директория ${MONITOR_INSTALL_DIR} успешно создана."
+    echo "  Директория ${MONITOR_INSTALL_DIR} успешно создана."
 else
-    echo "Директория ${MONITOR_INSTALL_DIR} уже существует."
+    echo "  Директория ${MONITOR_INSTALL_DIR} уже существует."
 fi
 
 # 2. Переходим в директорию установки
-cd "$MONITOR_INSTALL_DIR" || { echo "Ошибка: Не удалось перейти в директорию ${MONITOR_INSTALL_DIR}."; exit 1; }
+cd "$MONITOR_INSTALL_DIR" || { echo "  Ошибка: Не удалось перейти в директорию ${MONITOR_INSTALL_DIR}."; exit 1; }
 
 # 3. Скачиваем основной скрипт монитора
-echo "Скачиваем основной скрипт монитора с github..."
-curl -sL "$MONITOR_SCRIPT_URL" -o "$MONITOR_SCRIPT_NAME" || { echo "Ошибка: Не удалось скачать скрипт монитора."; exit 1; }
+echo "  Скачиваем основной скрипт монитора с github..."
+curl -sL "$MONITOR_SCRIPT_URL" -o "$MONITOR_SCRIPT_NAME" || { echo "  Ошибка: Не удалось скачать скрипт монитора."; exit 1; }
 chmod +x "$MONITOR_SCRIPT_NAME" # Делаем его исполняемым
-echo "Скрипт ${MONITOR_SCRIPT_NAME} успешно скачан и настроен."
+echo "  Скрипт ${MONITOR_SCRIPT_NAME} успешно скачан и настроен."
 
 echo ""
-echo "Установка завершена! Теперь вам необходимо выполнить следующие шаги:"
-echo -e "\n1. Откройте скачанный скрипт: \e[93mnano ${MONITOR_INSTALL_DIR}/${MONITOR_SCRIPT_NAME}\e[0m"
-echo -e "\n2. Измените в нем следующие переменные вручную:"
-echo "   - TELEGRAM_BOT_TOKEN"
-echo "   - TELEGRAM_ALERT_CHAT_IDS"
-echo "   - TELEGRAM_REPORT_CHAT_IDS"
-echo "   - TELEGRAM_INFO_CHAT_IDS"
-echo "   - USER_TO_PING (для тегов в Telegram, например, @YourUsername)"
-echo -e "   - BASE_USER_HOME (абсолютный путь к домашнему каталогу пользователя, где лежат бинарники нод\n    и .home директории, например, /home/Your_User_Name)"
-echo "     В данном случае, если вы запускаете скрипт от пользователя 'lilfox', то значением будет /home/lilfox."
-echo -e "\n3. Добавьте конфигурации для ваших сетей в ассоциативный массив NETWORKS."
-echo -e "\n4. Добавьте задание в cron для пользователя, от которого хотите запускать монитор\n   (обычно это ваш текущий пользователь):"
-echo -e "\e[93m   crontab -e\e[0m"
-echo "   Добавьте строку (например, для запуска каждые 10 минут):"
-echo "   */10 * * * * ${MONITOR_INSTALL_DIR}/${MONITOR_SCRIPT_NAME}"
+echo "  Установка завершена! Теперь вам необходимо выполнить следующие шаги:"
+echo -e "\n  1. Откройте скачанный скрипт: \e[93mnano ${MONITOR_INSTALL_DIR}/${MONITOR_SCRIPT_NAME}\e[0m"
+echo -e "\n  2. Измените в нем следующие переменные вручную:"
+echo "     - TELEGRAM_BOT_TOKEN"
+echo "     - TELEGRAM_ALERT_CHAT_IDS"
+echo "     - TELEGRAM_REPORT_CHAT_IDS"
+echo "     - TELEGRAM_INFO_CHAT_IDS"
+echo "     - USER_TO_PING (для тегов в Telegram, например, @YourUsername)"
+echo -e "     - BASE_USER_HOME (абсолютный путь к домашнему каталогу пользователя, где лежат бинарники нод\n      и .home директории, например, /home/Your_User_Name)"
+echo "       В данном случае, если вы запускаете скрипт от пользователя 'lilfox', то значением будет /home/lilfox."
+echo -e "\n  3. Добавьте конфигурации для ваших сетей в ассоциативный массив NETWORKS."
+echo -e "\n  4. Добавьте задание в cron для пользователя, от которого хотите запускать монитор\n     (обычно это ваш текущий пользователь):"
+echo -e "\e[93m     crontab -e\e[0m"
+echo "     Добавьте строку (например, для запуска каждые 10 минут):"
+echo "     */10 * * * * ${MONITOR_INSTALL_DIR}/${MONITOR_SCRIPT_NAME}"
 echo ""
-echo "Для тестирования вручную: ${MONITOR_INSTALL_DIR}/${MONITOR_SCRIPT_NAME} --debug"
+echo "  Для тестирования вручную: ${MONITOR_INSTALL_DIR}/${MONITOR_SCRIPT_NAME} --debug"
 
-echo -e "\n\e[93mОбратите внимание на краткую инструкцию приведённую выше!\n\e[0m"
+echo -e "\n\e[93m  Обратите внимание на краткую инструкцию приведённую выше!\n\e[0m"
 
 exit 0
